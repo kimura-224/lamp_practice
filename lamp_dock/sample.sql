0a16b0188ce8fe33b4deb5dbd4dcc9a3933f532b
kimura <ohara1765334@gmail.com>
2020-06-15 02:05:36 +0900

    テーブルへの追加

--- lamp_dock/sample.sql
+++ lamp_dock/sample.sql
@@ -1,16 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: mysql
--- 生成日時: 2019 年 8 月 16 日 06:28
--- サーバのバージョン： 5.7.27
--- PHP のバージョン: 7.2.19
+-- 生成日時: 2020 年 6 月 14 日 16:54
+-- サーバのバージョン： 5.7.30
+-- PHP のバージョン: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- データベース: `sample`
@@ -30,6 +35,13 @@ CREATE TABLE `carts` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `carts`
+--
+
+INSERT INTO `carts` (`cart_id`, `user_id`, `item_id`, `amount`, `created`, `updated`) VALUES
+(2, 4, 34, 5, '2020-06-06 12:54:40', '2020-06-08 23:44:37');
 
 -- --------------------------------------------------------
 
@@ -54,7 +66,34 @@ CREATE TABLE `items` (
 
 INSERT INTO `items` (`item_id`, `name`, `stock`, `price`, `image`, `status`, `created`, `updated`) VALUES
 (32, 'ねこ', 4, 30000, 'ny1owjn3yqs0cow8w4ws.jpg', 1, '2019-08-09 09:12:30', '2019-08-09 11:10:47'),
-(33, 'ハリネズミ', 30, 50000, '16scmunsexdwcosw88g0.jpg', 1, '2019-08-09 09:13:33', '2019-08-09 09:13:33');
+(33, 'ハリネズミ', 30, 50000, '16scmunsexdwcosw88g0.jpg', 1, '2019-08-09 09:13:33', '2019-08-09 09:13:33'),
+(34, 'hacked', 3, 18000, '681fpxh1xakosogggkws.jpg', 1, '2020-05-30 21:56:20', '2020-06-06 12:54:32');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `Purchase details`
+--
+
+CREATE TABLE `Purchase details` (
+  `item_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `Purchase history`
+--
+
+CREATE TABLE `Purchase history` (
+  `order number` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `create_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -98,6 +137,12 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`item_id`);
 
 --
+-- テーブルのインデックス `Purchase history`
+--
+ALTER TABLE `Purchase history`
+  ADD PRIMARY KEY (`order number`);
+
+--
 -- テーブルのインデックス `users`
 --
 ALTER TABLE `users`
@@ -111,7 +156,7 @@ ALTER TABLE `users`
 -- テーブルのAUTO_INCREMENT `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- テーブルのAUTO_INCREMENT `items`
@@ -120,10 +165,16 @@ ALTER TABLE `items`
   MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
+-- テーブルのAUTO_INCREMENT `Purchase history`
+--
+ALTER TABLE `Purchase history`
+  MODIFY `order number` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- テーブルのAUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- ダンプしたテーブルの制約
@@ -136,3 +187,7 @@ ALTER TABLE `carts`
   ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
